cc176af0eff3fc608e6fefb274447c0662ad9a84
impanikaa <panicka144@gmail.com>
2024-04-02 23:04:42 +0300

    футер готов + отредактирован хэдер

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,16 +1,24 @@
 import styles from './style.module.css';
 import { Link } from 'react-router-dom';
+import Picture_1 from '../../data/header_footer/Picture_1.png'
+import logoImage from '../../data/header_footer/Logo.png'
 
 const Footer = () => {
   return (
     <div className={styles.container}>
       <nav className={styles.navbar}>
-        <Link to='/' className={styles.links}>Главная</Link>
-        <Link to='/fipi' className={styles.links}>ФИПИ</Link>
+        <Link to='/' className={styles.links}>На главную</Link>
+        <Link to='/about' className={styles.links}>О сайте</Link>
         <Link to='/info' className={styles.links}>Об экзамене</Link>
         <Link to='/experimental-task' className={styles.links}>Задание №17</Link>
-        <Link to='/resources' className={styles.links}>Ресурсы</Link>
+        <Link to='/resources' className={styles.links}>Ресурсы для подготовки</Link>
       </nav>
+      <div className={styles.line}></div>
+      <img src={Picture_1} className={styles.picture} alt='картинка'></img>
+      <div className={styles.text_logo}>
+        <p className={styles.text}>Смело. Ответственно. Креативно.</p>
+        <Link to='/'><img src={logoImage} alt="Логотип" className={styles.logo} /></Link>
+      </div>
     </div>
   );
 };
