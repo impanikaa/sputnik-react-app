673b2bcbc718c1550b76121d7671d15e11e9ef6c
impanikaa <panicka144@gmail.com>
2024-04-05 23:00:50 +0300

    добавлен импорт разных шрифтов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import './App.css';
+import './data/fonts/Roboto-Medium.ttf'
+import './data/fonts/Roboto-Light.ttf'
+import './data/fonts/Roboto-Regular.ttf'
+import './data/fonts/RobotoCondensed-Regular.ttf'
+import './data/fonts/RobotoSlab-Medium.ttf'
+import './data/fonts/RobotoSlab-Regular.ttf'
 import Header from './components/header';
 import Main from './components/main';
 import Footer from './components/footer';
