27f69c436312f2cb82e8823a726d198ffb9cab59
impanikaa <panicka144@gmail.com>
2024-04-06 00:05:34 +0300

    главный экран на главной странице (без кнопки)

--- src/components/home/index.jsx
+++ src/components/home/index.jsx
@@ -5,7 +5,16 @@ const Home = () => {
     return (
     <div className={styles.container}>
         <div className={styles.container_main}>
-            <img src={Picture_1} className={styles.logo}></img>
+            <div className={styles.main_text}>
+                <h1>Подготовка к</h1>
+                <h1>ОГЭ по физике</h1>
+                <h2>2023-2024</h2>
+                <p>Здесь собрана вся нужная информация об экзамене и 
+                    материалы для подготовки, которые помогут успешно и
+                    без лишнего стресса сдать экзамен по одному из самых
+                    сложных предметов</p>
+            </div>
+            <img src={Picture_1} className={styles.picture_1_main}></img>
         </div>
     </div>
     );
