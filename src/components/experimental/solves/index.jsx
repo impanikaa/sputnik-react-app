3a39485154c82145c6724a9d0cbb9ba7d0e19a22
impanikaa <panicka144@gmail.com>
2024-04-15 00:33:21 +0300

    страница решений задания 17 частично готова

--- src/components/experimental/solves/index.jsx
+++ src/components/experimental/solves/index.jsx
@@ -87,7 +87,7 @@ const Solves = () => {
                   <div className={styles.number}>1</div>
                   Таблица с анализом условия задачи
                   <button classname={styles.Btn}>
-                    <a href={documentsData[selectedTask].link_table} target="_blank" rel="noreferrer">
+                    {/* <a href={documentsData[selectedTask].link_table} target="_blank" rel="noreferrer"> */}
                     <svg classname={styles.svgIcon} viewBox="0 0 384 512" height="1em" 
                     xmlns="http://www.w3.org/2000/svg">
                       <path d="M169.4 470.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 
@@ -95,7 +95,7 @@ const Solves = () => {
                       0L224 370.8 224 64c0-17.7-14.3-32-32-32s-32 14.3-32 32l0 306.7L54.6 
                       265.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"></path></svg>
                     <span classname={styles.icon2}></span>
-                    </a>
+                    {/* </a> */}
                   </button>
                 </div>
               </div>
