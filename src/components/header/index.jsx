868f9aaadd79894bfdf00dba9fa223a30b0d2ae4
impanikaa <panicka144@gmail.com>
2024-06-29 22:28:52 +0300

    Улучшена адаптивность v2

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -27,11 +27,11 @@ const Header = () => {
         </label>
         <nav className={`${styles.navbarLinks} ${isMenuOpen ? styles.open : ''}`}>
           <Link to='/' className={styles.links} onClick={() => setIsMenuOpen(false)}>Главная</Link>
-          <Link to='/fipi' className={styles.links} onClick={() => setIsMenuOpen(false)}>ФИПИ</Link>
-          <Link to='/info' className={styles.links} onClick={() => setIsMenuOpen(false)}>Об экзамене</Link>
+          <Link to='/lost' className={styles.links} onClick={() => setIsMenuOpen(false)}>ФИПИ</Link>
+          <Link to='/lost' className={styles.links} onClick={() => setIsMenuOpen(false)}>Об экзамене</Link>
           <Link to='/experimental-task' className={styles.links} onClick={() => setIsMenuOpen(false)}>Задание №17</Link>
           <Link to='/resources' className={styles.links} onClick={() => setIsMenuOpen(false)}>Ресурсы</Link>
-          <Link to='/about' className={styles.links} onClick={() => setIsMenuOpen(false)}>О сайте</Link>
+          <Link to='/lost' className={styles.links} onClick={() => setIsMenuOpen(false)}>О сайте</Link>
         </nav>
       </div>
       </div>
--- src/components/lost/index.jsx
+++ src/components/lost/index.jsx
@@ -3,7 +3,7 @@ import styles from './style.module.css'
 const Lost = () => {
     return (
     <div className={styles.container}>
-        <h1>На этой странице пока ничего нет, но скоро появится! :) </h1>
+        <h1 className={styles.text}>На этой странице пока ничего нет, ждите обновлений! :) </h1>
     </div>
     );
 }
