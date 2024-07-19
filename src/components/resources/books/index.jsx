3822488cfcd6be3a2378edfaaf22c55036b14452
impanikaa <panicka144@gmail.com>
2024-07-19 23:36:28 +0300

    Создана страница res/books

--- src/components/resources/books/index.jsx
+++ src/components/resources/books/index.jsx
@@ -1,9 +1,159 @@
 import styles from './style.module.css'
+import Planet from '../../../data/resources/planet.png'
+import Link from '../../../data/experimental/solves/Link.png';
 
 const BooksRes = () => {
     return (
     <div className={styles.container}>
-        <h1 className={styles.text}>На этой странице пока ничего нет, ждите обновлений! :) </h1>
+
+        <div className={styles.container_main}>
+            <div className={styles.main_text}>
+                <h1>Ресурсы: Литература</h1>
+            </div>
+            <img src={Planet}></img>
+        </div>
+
+        <div className={styles.links_block}>
+
+        <div className={styles.links_block_1}>
+
+        <div className={styles.link}>
+            <p>Белага - Физика, 7 класс</p>
+            
+                <a href='https://www.litres.ru/book/v-v-belaga/fizika-7-klass-68297828/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+            
+        </div>
+
+        <div className={styles.link}>
+            <p>Белага - Физика, 8 класс</p>
+            
+                <a href='https://www.litres.ru/book/v-v-belaga/fizika-8-klass-68297846/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Белага - Физика, 9 класс</p>
+                <a href='https://www.litres.ru/book/v-v-belaga/fizika-9-klass-68297864/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Кикоин - Физика 8 класс</p>
+                <a href='https://vk.com/wall-128917207_162' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Кикоин - Физика 9 класс</p>
+                <a href='https://27.edubish.kg/wp-content/uploads/sites/188/2021/10/fizika-9-kikoin.pdf' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Мякишев 1 - Механика</p>
+                <a href='https://www.litres.ru/book/aron-sinyakov/fizika-10-klass-mehanika-uglublennyy-uroven-68297210/?utm_source=yandex&utm_medium=cpc&utm_campaign=mixed_web_books_dsa_drr_feed+607619718%7C47441382&utm_content=8258473710&utm_term=&param_2=987239&yclid=10935567623396261887&lfrom_processed=607619718' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Мякишев 2 - Термодинамика, Молекулярная физика</p>
+                <a href='https://www.labirint.ru/books/758662/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Мякишев 3 - Электродинамика</p>
+                <a href='https://fizmat.space/library-files/Физика-Электродинамика-Мякишев.pdf' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Мякишев 4 - Колебания и волны</p>
+                <a href='https://fizmat.space/library-files/Физика-Колебания-и-волны-Мякишев.pdf' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        </div>
+
+        <div className={styles.links_block_1}>
+
+        <div className={styles.link}>
+            <p>Мякишев 5 - Оптика и квантовая физика</p>
+                <a href='https://www.litres.ru/book/aron-sinyakov/fizika-optika-kvantovaya-fizika-11-klass-uglublennyy-uroven-68297252/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Замятнин - 7 класс, Основы механики</p>
+                <a href='https://www.ozon.ru/product/sbornik-zadach-po-fizike-7-klass-osnovy-mehaniki-m-yu-zamyatnin-zamyatnin-mihail-yurevich-498427044/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Замятнин - 8 класс</p>
+                <a href='https://www.ozon.ru/product/sbornik-zadach-po-fizike-8-klass-teplovye-yavleniya-postoyannyy-tok-optika-m-yu-zamyatnin-zamyatnin-498428091/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Замятнин - 9 класс, 1 том</p>
+                <a href='https://www.ozon.ru/product/sbornik-zadach-dlya-podgotovki-k-olimpiadam-po-fizike-mehanika-9-klass-kinematika-498426082/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Замятнин - 9 класс, 2 том</p>
+                <a href='https://www.ozon.ru/product/sbornik-zadach-po-fizike-9-klass-mehanika-dinamika-statika-zakony-sohraneniya-tom-2-zamyatnin-498430181/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Бутиков, Кондратьев - Механика</p>
+                <a href='https://www.litres.ru/book/a-s-kondratev/fizika-kniga-1-mehanika-18401862/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Бутиков, Кондратьев - Строение и свойства вещества</p>
+                <a href='https://www.litres.ru/book/a-s-kondratev/fizika-kniga-3-stroenie-i-svoystva-veschestva-5822562/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Бутиков, Кондратьев - Электродинамика и оптика</p>
+                <a href='https://www.litres.ru/book/a-s-kondratev/fizika-kniga-2-elektrodinamika-optika-18401799/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        <div className={styles.link}>
+            <p>Савченко - Задачи по физике</p>
+                <a href='https://www.ozon.ru/product/zadachi-po-fizike-savchenko-oliver-yakovlevich-kutuzova-galina-aleksandrovna-1409541162/' target="_blank" rel="noreferrer">
+                <button classname={styles.Btn}><img src={Link}></img></button>
+                </a>
+        </div>
+
+        </div>
+
+        </div>
+
     </div>
     );
 }
